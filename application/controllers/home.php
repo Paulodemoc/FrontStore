41074cff38e82247f40460353c852ecda2792549
Paulo Henrique Vieira Neves <paulodemoc@gmail.com>
2018-01-24 11:35:44 -0200

    Layout adjustmentst Facebook login

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,19 +1,27 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+define("app_id", "1795623014075486");
+define("app_secret", "a96ced6d1db82bf2add3eb2b36db996d");
+
 class Home extends CI_Controller {
 	public function index()
 	{
 		$data = array();
-		$this->template->set('title', 'Home');
-		$this->template->load('default', 'contents' , 'home', $data);
+		$this->layout->set('title', 'Home');
+
+		//todo: get products
+		$products = array();
+		$data['products'] = $products;
+
+		$this->layout->load('default', 'contents' , 'home', $data);
 	}
 
 	public function login()
 	{
 		$fb = new Facebook\Facebook([
-			'app_id' => '{app-id}',
-			'app_secret' => '{app-secret}',
+			'app_id' => app_id,
+			'app_secret' => app_secret,
 			'default_graph_version' => 'v2.2',
 			]);
 		  
@@ -24,76 +32,78 @@ class Home extends CI_Controller {
 		  redirect($loginUrl, 'refresh');
 	}
 
+	public function logout(){
+		$this->session->unset_userdata('fb_access_token');
+		$this->session->unset_userdata('loggedUser');
+		redirect('home/index','refresh');
+	}
+
 	public function loginCallback(){
 		$fb = new Facebook\Facebook([
-			'app_id' => '{app-id}',
-			'app_secret' => '{app-secret}',
+			'app_id' => app_id,
+			'app_secret' => app_secret,
 			'default_graph_version' => 'v2.2',
 			]);
 		  
 		  $helper = $fb->getRedirectLoginHelper();
 		  
 		  try {
-			$accessToken = $helper->getAccessToken();
+				$accessToken = $helper->getAccessToken();
 		  } catch(Facebook\Exceptions\FacebookResponseException $e) {
-			// When Graph returns an error
-			echo 'Graph returned an error: ' . $e->getMessage();
-			exit;
-		  } catch(Facebook\Exceptions\FacebookSDKException $e) {
-			// When validation fails or other local issues
-			echo 'Facebook SDK returned an error: ' . $e->getMessage();
-			exit;
+				// When Graph returns an error
+			} catch(Facebook\Exceptions\FacebookSDKException $e) {
+				// When validation fails or other local issues
 		  }
 		  
 		  if (! isset($accessToken)) {
-			if ($helper->getError()) {
-			  header('HTTP/1.0 401 Unauthorized');
-			  echo "Error: " . $helper->getError() . "\n";
-			  echo "Error Code: " . $helper->getErrorCode() . "\n";
-			  echo "Error Reason: " . $helper->getErrorReason() . "\n";
-			  echo "Error Description: " . $helper->getErrorDescription() . "\n";
-			} else {
-			  header('HTTP/1.0 400 Bad Request');
-			  echo 'Bad request';
-			}
-			exit;
+				if ($helper->getError()) {
+					header('HTTP/1.0 401 Unauthorized');
+					echo "Error: " . $helper->getError() . "\n";
+					echo "Error Code: " . $helper->getErrorCode() . "\n";
+					echo "Error Reason: " . $helper->getErrorReason() . "\n";
+					echo "Error Description: " . $helper->getErrorDescription() . "\n";
+				} else {
+					header('HTTP/1.0 400 Bad Request');
+					echo 'Bad request';
+				}
+				exit;
 		  }
-		  
-		  // Logged in
-		  echo '<h3>Access Token</h3>';
-		  var_dump($accessToken->getValue());
-		  
+
 		  // The OAuth 2.0 client handler helps us manage access tokens
 		  $oAuth2Client = $fb->getOAuth2Client();
 		  
 		  // Get the access token metadata from /debug_token
 		  $tokenMetadata = $oAuth2Client->debugToken($accessToken);
-		  echo '<h3>Metadata</h3>';
-		  var_dump($tokenMetadata);
 		  
 		  // Validation (these will throw FacebookSDKException's when they fail)
-		  $tokenMetadata->validateAppId('{app-id}'); // Replace {app-id} with your app id
+		  $tokenMetadata->validateAppId(app_id);
 		  // If you know the user ID this access token belongs to, you can validate it here
 		  //$tokenMetadata->validateUserId('123');
 		  $tokenMetadata->validateExpiration();
 		  
 		  if (! $accessToken->isLongLived()) {
-			// Exchanges a short-lived access token for a long-lived one
-			try {
-			  $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-			} catch (Facebook\Exceptions\FacebookSDKException $e) {
-			  echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
-			  exit;
-			}
-		  
-			echo '<h3>Long-lived</h3>';
-			var_dump($accessToken->getValue());
+				// Exchanges a short-lived access token for a long-lived one
+				try {
+					$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+				} catch (Facebook\Exceptions\FacebookSDKException $e) {
+					exit;
+				}
 		  }
-		  
-		  $_SESSION['fb_access_token'] = (string) $accessToken;
-		  
-		  // User is logged in with a long-lived access token.
-		  // You can redirect them to a members-only page.
-		  //header('Location: https://example.com/members.php');
+			
+			$this->session->set_userdata('fb_access_token',(string) $accessToken);
+
+			try {
+				$response = $fb->get('/me?fields=id,name', $accessToken);
+			} catch(Facebook\Exceptions\FacebookResponseException $e) {
+				
+			} catch(Facebook\Exceptions\FacebookSDKException $e) {
+				
+			}
+			
+			$user = $response->getGraphUser();
+
+			$this->session->set_userdata('loggedUser', $user->getName());
+	  
+		  redirect('home/index','refresh');
 	}
 }
--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -3,48 +3,41 @@
     <title><?php echo $title ?></title>
 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <link href="<?php echo base_url();?>css/app.css" rel="stylesheet">
 </head>
 <body>
 
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Front Store</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-		</ul>
-		<form class="form-inline my-2 my-lg-0">	
-			<?php echo anchor('home/login', 
-				img('https://scontent.frao1-2.fna.fbcdn.net/v/t39.2365-6/17639236_1785253958471956_282550797298827264_n.png?oh=8ba4730dd8728f6fc0a63c7c6f924739&oe=5AD919EA', 
-					FALSE,
-					array('alt' => 'Login with Facebook',
-					'width' => '100')
-				)
-			); ?>
-		</form>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <!-- <li class="nav-item active">
+            <?php echo anchor('home','Home',array('class'=>'nav-link')); ?>
+          </li> -->
+		    </ul>
+      <?php if(!$this->session->has_userdata('fb_access_token')): ?>
+        <form class="form-inline my-2 my-lg-0">	
+          <?php echo anchor('home/login', 
+            img('https://scontent.frao1-2.fna.fbcdn.net/v/t39.2365-6/17639236_1785253958471956_282550797298827264_n.png?oh=8ba4730dd8728f6fc0a63c7c6f924739&oe=5AD919EA', 
+              FALSE,
+              array('alt' => 'Login with Facebook',
+              'width' => '250')
+            )
+          ); ?>
         </form>
+      <?php endif; ?>
+      <?php if($this->session->has_userdata('loggedUser')): ?>
+        <div class="nav-item dropdown my-2 my-lg-0" style="color:white;">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdownUser" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $this->session->userdata('loggedUser'); ?></a>
+          <div class="dropdown-menu" aria-labelledby="dropdownUser">
+              <?php echo anchor('home/logout','Sign Out',array('class'=>'dropdown-item')); ?>
+          </div>
+        </div>
+      <?php endif; ?>
       </div>
     </nav>
 
@@ -53,6 +46,8 @@
 	</main><!-- /.container -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  <script src="<?php echo base_url();?>js/popper.min.js" ></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
   </body>
 </html>
